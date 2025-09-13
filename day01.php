95b6f56de94f9e199961c618718dede0a39ba25a
oey <134188837+oeyo-std@users.noreply.github.com>
2025-09-13 23:04:56 +0800

    PHP Day 01

---
+++ day01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Day 01</title>
+</head>
+<body>
+    <?php
+
+        $crush = "Claire Mendoza";
+        
+        $i = 1;
+        while($i <= 10){
+            echo "I Love You $crush $i x <br>";
+            $i++;
+        }
+    ?>
+</body>
+</html>
