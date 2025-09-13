41d57a729ea2910d9c9f307eb509c63331f91e06
oey <134188837+oeyo-std@users.noreply.github.com>
2025-09-13 23:28:56 +0800

    added  Problem Solving #1

--- day01.php
+++ day01.php
@@ -1,22 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Day 01</title>
 </head>
+
 <body>
     <?php
-    
-        //Data Declaration
-        $crush = "Claire Mendoza";
-        
-        //While Loop
-        $i = 1;
-        while($i <= 10){
-            echo "I Love You $crush $i x <br>";
-            $i++;
+
+    //Data Declaration
+    $crush = "Claire Mendoza";
+
+    //While Loop
+    $i = 1;
+    while ($i <= 10) {
+        echo "I Love You $crush $i x <br>";
+        $i++;
+    }
+
+    echo "<br>";
+
+    //Problem Solving #1
+
+    $num = 1;
+    $tempTotal = 0;
+
+    while ($num <= 50) {
+
+        if ($num % 2 == 0) {
+            $tempTotal += $num;
         }
+
+        $num++;
+    }
+
+    echo "The sum of even numbers from 1 to 50 is " . $tempTotal;
     ?>
 </body>
+
 </html>
